1ab90b4581f0cb842edd34408464fbeacf4adcfc
Anurag Satapathy <anurag.satapathy401@gmail.com>
2025-06-18 09:00:50 +0530

    employee crud operations

--- src/main/java/com/boot/employee/controller/EmployeeController.java
+++ src/main/java/com/boot/employee/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.boot.employee.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +37,30 @@ public class EmployeeController {
 	public Employee addEmployee(@RequestBody Employee employee) {
 		return repository.save(employee);
 		
+	}
+	
+	//get employee by id
+	
+	@GetMapping("/{id}")
+	public Optional<Employee> getEmployee(@PathVariable Integer id){
+		return repository.findById(id);
+		
+	}
+	
+	//update student by id 
+	
+	@PutMapping("/{id}")
+	public Employee updateEmployee(@PathVariable Integer id, @RequestBody Employee Employeedetails) {
+		Employee employee = repository.findById(id).orElseThrow();
+		employee.setName(Employeedetails.getName());
+		employee.setEmail(Employeedetails.getEmail());
+		return repository.save(employee);
+		
+	}
+	
+	//delete student by id 
+	@DeleteMapping("/{id}")
+	public void deleteEmployee(@PathVariable Integer id) {
+		repository.deleteById(id);
 	}
 }
